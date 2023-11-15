1f0654e5a2046ea7739f28bd102422651dfe4a55
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-15 20:40:53 +0600

    Add features configuration file.

---
+++ src/config/features.ts
@@ -0,0 +1,34 @@
+import type { CardInfo } from "@/types";
+
+export const features: CardInfo[] = [
+  {
+    title: "Lunar 3D Mapping",
+    description: "Dive into a high-resolution 3D map of the Moon, offering unparalleled detail and accuracy. Plan your missions with precision, leveraging real lunar spatial data for an immersive experience.",
+    image: "/features/lunar-3d-mapping.png",
+  },
+  {
+    title: "Collaborative Mission Design",
+    description: "Work in real-time with fellow space engineers from around the world. Share, discuss, and refine your lunar mission designs in a collaborative metaverse, powered by web3 tools.",
+    image: "/features/collab.png",
+  },
+  {
+    title: "IP-NFT for Designs",
+    description: "Protect and monetize your innovative space mission designs by issuing them as Intellectual Property Non-Fungible Tokens (IP-NFTs). Showcase your expertise and gain recognition in the space engineering community.",
+    image: "/features/ip-nft.png",
+  },
+  {
+    title: "Decentralized Engineer Profiles",
+    description: "Create your decentralized engineer profile. Manage access, showcase your projects, and connect with peers in a secure and transparent manner.",
+    image: "/features/decentralized.png",
+  },
+  {
+    title: "Interactive Training Modules",
+    description: "Engage in hands-on training sessions within the LunCo platform. Simulate real-world lunar scenarios, test your designs, and receive instant feedback, all within a dynamic and interactive environment.",
+    image: "/features/trainding-modules.png",
+  },
+  {
+    title: "Open-Source",
+    description: "LunCo is open-source and free to use for everyone.",
+    image: "/features/open-source.png",
+  }
+]
