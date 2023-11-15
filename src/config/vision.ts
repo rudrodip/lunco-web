8325fafd2e62102ce014e56807805b50d169e01c
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-15 20:41:02 +0600

    Add vision configuration file.

---
+++ src/config/vision.ts
@@ -0,0 +1,34 @@
+import type { CardInfo } from "@/types";
+
+export const visions: CardInfo[] = [
+  {
+    title: "Unified Platform",
+    description: "LunCo serves as a central hub, bringing together the best of open-source space engineering tools, offering a unified experience for users.",
+    image: "/visions/unified-platform.png",
+  },
+  {
+    title: "Requirements Management",
+    description: "Streamline and manage your project requirements with ease.",
+    image: "/visions/requirements-management.png",
+  },
+  {
+    title: "Models Visualizations",
+    description: "Visualize and interact with your designs in a 3D environment.",
+    image: "/visions/models-visualizations.png",
+  },
+  {
+    title: "Collaborative Training",
+    description: "Train and collaborate with your team in real-time.",
+    image: "/visions/collaborative-training.png",
+  },
+  {
+    title: "Digital Twin",
+    description: "Create a digital replica of your lunar colony for simulations and analysis.",
+    image: "/visions/digital-twin.png",
+  },
+  {
+    title: "Open-Source",
+    description: "LunCo is open-source and free to use for everyone.",
+    image: "/visions/open-source.png",
+  }
+]
