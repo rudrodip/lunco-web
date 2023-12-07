014d12b1250a5e7f6c3acb097ceab97cb333a8fb
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-07 12:47:34 +0600

    Update VisionCard component styling

--- src/components/vision/vision-card.tsx
+++ src/components/vision/vision-card.tsx
@@ -11,30 +11,32 @@ type VisionCardProps = {
 export default function VisionCard({ vision }: VisionCardProps) {
   return (
     <Tilt
-      tiltMaxAngleX={5}
-      tiltMaxAngleY={5}
+      tiltMaxAngleX={3.5}
+      tiltMaxAngleY={3.5}
+      glareReverse
       glareEnable
       glareColor="#34d2eb"
       glareMaxOpacity={0.2}
       glarePosition="all"
       glareBorderRadius="10px"
+      scale={1.01}
       className="mx-1"
     >
       <Card
         isFooterBlurred
         radius="lg"
-        className="border-none bg-primary-foreground/10 backdrop-blur-sm h-full"
+        className="border bg-primary-foreground/10 backdrop-blur-sm h-full"
       >
         <Image
+          src={vision.image}
           alt={vision.title}
           className="object-cover"
           height={300}
-          src={vision.image}
           width={500}
         />
-        <CardFooter className="flex-col justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <h1 className="font-heading text-lg">{vision.title}</h1>
-          <p className="text-sm text-white/80">{vision.description}</p>
+        <CardFooter className="flex-col justify-between overflow-hidden py-1 absolute rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <h1 className="font-heading text-xl">{vision.title}</h1>
+          <p className="text-lg">{vision.description}</p>
         </CardFooter>
       </Card>
     </Tilt>
