072965a2136e5fce0a8ca543b02f2203c05db9f8
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-07 12:47:41 +0600

    Update feature images in config file

--- src/config/features.ts
+++ src/config/features.ts
@@ -3,32 +3,37 @@ import type { CardInfo } from "@/types";
 export const features: CardInfo[] = [
   {
     title: "Lunar 3D Mapping",
-    description: "Dive into a high-resolution 3D map of the Moon, offering unparalleled detail and accuracy. Plan your missions with precision, leveraging real lunar spatial data for an immersive experience.",
-    image: "/features/lunar-3d-mapping.png",
+    description:
+      "Dive into a high-resolution 3D map of the Moon, offering unparalleled detail and accuracy. Plan your missions with precision, leveraging real lunar spatial data for an immersive experience.",
+    image: "/features/feature1.png",
   },
   {
     title: "Collaborative Mission Design",
-    description: "Work in real-time with fellow space engineers from around the world. Share, discuss, and refine your lunar mission designs in a collaborative metaverse, powered by web3 tools.",
-    image: "/features/collab.png",
+    description:
+      "Work in real-time with fellow space engineers from around the world. Share, discuss, and refine your lunar mission designs in a collaborative metaverse, powered by web3 tools.",
+    image: "/features/feature2.png",
   },
   {
     title: "IP-NFT for Designs",
-    description: "Protect and monetize your innovative space mission designs by issuing them as Intellectual Property Non-Fungible Tokens (IP-NFTs). Showcase your expertise and gain recognition in the space engineering community.",
-    image: "/features/ip-nft.png",
+    description:
+      "Protect and monetize your innovative space mission designs by issuing them as Intellectual Property Non-Fungible Tokens (IP-NFTs). Showcase your expertise and gain recognition in the space engineering community.",
+    image: "/features/feature3.png",
   },
   {
     title: "Decentralized Engineer Profiles",
-    description: "Create your decentralized engineer profile. Manage access, showcase your projects, and connect with peers in a secure and transparent manner.",
-    image: "/features/decentralized.png",
+    description:
+      "Create your decentralized engineer profile. Manage access, showcase your projects, and connect with peers in a secure and transparent manner.",
+    image: "/features/feature4.png",
   },
   {
     title: "Interactive Training Modules",
-    description: "Engage in hands-on training sessions within the LunCo platform. Simulate real-world lunar scenarios, test your designs, and receive instant feedback, all within a dynamic and interactive environment.",
-    image: "/features/trainding-modules.png",
+    description:
+      "Engage in hands-on training sessions within the LunCo platform. Simulate real-world lunar scenarios, test your designs, and receive instant feedback, all within a dynamic and interactive environment.",
+    image: "/features/feature5.png",
   },
   {
     title: "Open-Source",
     description: "LunCo is open-source and free to use for everyone.",
-    image: "/features/open-source.png",
-  }
-]
+    image: "/features/feature6.png",
+  },
+];
