031908e4d53f009e097f8cd17dd540d66a34a716
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-07 12:47:38 +0600

    Update vision section layout

--- src/components/vision/vision.tsx
+++ src/components/vision/vision.tsx
@@ -9,21 +9,20 @@ const DynamicMoon = dynamic(() => import("@/components/three-scenes/moon"), {
 export default function Vision() {
   return (
     <section id="vision" className="relative my-6 lg:my-32 min-h-screen">
-      <div className="absolute inset-0 z-[-1] w-full h-[30%] lg:h-auto -translate-y-[15%] lg:-translate-y-[20%] lg:-translate-x-[70%]">
+      <div className="absolute inset-0 z-[-1] w-full h-[30%] lg:h-auto -translate-y-[15%] lg:-translate-y-[20%] lg:-translate-x-[20%]">
         <DynamicMoon />
       </div>
       <div>
         <h1 className="head-text primary-gradient">Our Vision</h1>
         <p className="text-2xl max-w-lg">
-          Our vision is to provide a comprehensive suite of open-source applications
-          tailored for Lunar Base engineering
+          Our vision is to provide a comprehensive suite of open-source
+          applications tailored for Lunar Base engineering
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 my-5">
-        {visions
-          .map((vision, i) => {
-            return <VisionCard key={i} vision={vision} />;
-          })}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 my-5">
+        {visions.map((vision, i) => {
+          return <VisionCard key={i} vision={vision} />;
+        })}
       </div>
     </section>
   );
